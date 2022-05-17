ce9ccbe76dc5cc903897e0de0282de1be599aa2b
VADLA-CHARY <76141959+VADLA-CHARY@users.noreply.github.com>
2022-05-17 13:56:32 +0530

    Create Hand Shake.c

---
+++ Hand Shake.c
@@ -0,0 +1,8 @@
+/*
+At the annual meeting of Board of Directors of Acme Inc. If everyone attending shakes hands exactly one time with every other attendee, how many handshakes are there?
+*/
+
+int handshake(int n) {
+    return (n*(n-1))/2;
+
+}
