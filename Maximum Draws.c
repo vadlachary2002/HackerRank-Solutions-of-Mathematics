1886cb9c64100a9f1fafe6ca7806f4ad02399ce0
VADLA-CHARY <76141959+VADLA-CHARY@users.noreply.github.com>
2022-05-17 13:55:48 +0530

    Create Maximum Draws.c

---
+++ Maximum Draws.c
@@ -0,0 +1,7 @@
+/*A person is getting ready to leave and needs a pair of matching socks.
+If there are  colors of socks in the drawer, how many socks need to be removed to be certain of having a matching pair?
+*/
+int maximumDraws(int n) {
+    return n+1;
+
+}
